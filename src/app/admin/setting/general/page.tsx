3df4d45e110152695ed1bbb10abf31888ea02a23
sauravphuyal2001 <sauravphuyal2001@gmail.com>
2025-11-18 12:54:44 +0545

    setting general pages

---
+++ src/app/admin/setting/general/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const generalpages = () => {
+  return (
+    <div>
+      this pages contains general setting/letterpad of websites.
+    </div>
+  )
+}
+
+export default generalpages
